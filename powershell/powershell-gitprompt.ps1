b6e6350bc75f72a34e228b1075506fc64c071456
User <waterns@users.noreply.github.com>
2018-10-03 13:19:46 -0600

    Powershell - Update Git Prompt

--- powershell/powershell-gitprompt.ps1
+++ powershell/powershell-gitprompt.ps1
@@ -1,30 +1,58 @@
 ﻿Function Prompt {
 
-# ToDO:
-# - Capture how many commits ahead
+$SYMBOL_GIT_BRANCH='⑂'
+$SYMBOL_GIT_MODIFIED='*'
+$SYMBOL_GIT_PUSH='↑'
+$SYMBOL_GIT_PULL='↓'
 
 if (Test-Path ".git" -PathType Container) {
-  $symbolicref = $(git symbolic-ref HEAD)
+
+  $symbolicref = $(git symbolic-ref --short HEAD 2>$NULL)
+
+  if ($symbolicref) {#For branches append symbol
+    $branch = $symbolicref.substring($symbolicref.LastIndexOf("/") +1)
+    $branchText=$SYMBOL_GIT_BRANCH + ' ' + $branch
+  } else {#otherwise use tag/SHA
+      $symbolicref=$(git describe --tags --always 2>$NULL)
+      $branch=$symbolicref
+      $branchText=$symbolicref
+  }
+
 } else {$symbolicref = $NULL}
 
 
 if ($symbolicref -ne $NULL) {
-  $branch = $symbolicref.substring($symbolicref.LastIndexOf("/") +1)
+  # Tweak: 
+  # When WSL and Powershell terminals concurrently viewing same repo
+  # Stops from showing CRLF/LF differences as updates
+  git status > $NULL
   
   $differences = $(git diff-index --name-status HEAD)
   
   If ($differences -ne $NULL) {
-    $localchanges = $true
     $git_create_count = [regex]::matches($differences, "A`t").count
     $git_update_count = [regex]::matches($differences, "M`t").count
     $git_delete_count = [regex]::matches($differences, "D`t").count
   }
   else {
-    $localchanges = $false
     $git_create_count = 0
     $git_update_count = 0
     $git_delete_count = 0
   }
+
+  #Identify how many commits ahead and behind we are
+  #by reading first two lines of `git status`
+  $marks=$NULL
+  (git status --porcelain --branch 2>$NULL) | ForEach-Object { 
+  
+      If ($_ -match '^##') {
+        If ($_ -match 'ahead\ ([0-9]+)') {$git_ahead_count=[int]$Matches[1]}
+        If ($_ -match 'behind\ ([0-9]+)') {$git_behind_count=[int]$Matches[1]}
+      }
+  }
+  $branchText+="$marks"
+
+
 }
 
 if (test-path variable:/PSDebugContext) { 
@@ -39,28 +67,34 @@ if ($symbolicref -ne $NULL) {
 
   #Output the branch in prettier colors
   If ($branch -eq "master") {
-    Write-Host ($branch) -nonewline -foregroundcolor White
+    Write-Host ($branchText) -nonewline -foregroundcolor White
   }
-  else {Write-Host $branch -nonewline -foregroundcolor Red}
+  else {Write-Host $branchText -nonewline -foregroundcolor Red}
+
+  #Output commits ahead/behind, in pretty colors
+  If ($git_ahead_count -gt 0) {
+      Write-Host (" $SYMBOL_GIT_PUSH") -nonewline -foregroundcolor White
+      Write-Host ($git_ahead_count) -nonewline -foregroundcolor Green
+  }
+  If ($git_behind_count -gt 0) {
+      Write-Host (" $SYMBOL_GIT_PULL") -nonewline -foregroundcolor White
+      Write-Host ($git_behind_count) -nonewline -foregroundcolor Yellow
+  }
   
+  #Output unstaged changes count, if any, in pretty colors   
+  If ($git_create_count -gt 0) {
+      Write-Host (" c:") -nonewline -foregroundcolor White
+      Write-Host ($git_create_count) -nonewline -foregroundcolor Green
+  }
   
-  #Output local changes count, if any, in pretty colors
-  If ($localchanges) {
-     
-      If ($git_create_count -gt 0) {
-          Write-Host (" c:") -nonewline -foregroundcolor White
-          Write-Host ($git_create_count) -nonewline -foregroundcolor Green
-      }
+  If ($git_update_count -gt 0) {
+    Write-Host (" u:") -nonewline -foregroundcolor White
+    Write-Host ($git_update_count) -nonewline -foregroundcolor Yellow
+  }
   
-      If ($git_update_count -gt 0) {
-        Write-Host (" u:") -nonewline -foregroundcolor White
-        Write-Host ($git_update_count) -nonewline -foregroundcolor Yellow
-      }
-  
-      If ($git_delete_count -gt 0) {
-        Write-Host (" d:") -nonewline -foregroundcolor White
-        Write-Host ($git_delete_count) -nonewline -foregroundcolor Red
-      }
+  If ($git_delete_count -gt 0) {
+    Write-Host (" d:") -nonewline -foregroundcolor White
+    Write-Host ($git_delete_count) -nonewline -foregroundcolor Red
   }
   
   Write-Host (" ]") -nonewline -foregroundcolor Magenta
@@ -71,5 +105,4 @@ $(Write-Host $('>' * ($nestedPromptLevel + 1)) -nonewline -foregroundcolor White
 
 
 
-return " "
-}
+return " "}#Powershell requires a return, otherwise defaults to factory prompt
