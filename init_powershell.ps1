372819ee15970b69cae49bd68a2d1abf6020ab87
User <waterns@users.noreply.github.com>
2018-11-20 11:33:38 -0700

    init-powershell - Update common aliases to use full command names (eg. `cd` to `Set-Location`

--- init_powershell.ps1
+++ init_powershell.ps1
@@ -1,4 +1,4 @@
-Param (
+﻿Param (
   [switch][Alias('u')]$update
 )
 
@@ -30,7 +30,7 @@ catch [System.Management.Automation.CommandNotFoundException]
 }
 
 if ($update) {
-  echo "UPDATING..."
+  Write-Output "UPDATING..."
   updategitrepo "dotfiles" "profile configs" "$HOMEREPO"
 }
 
@@ -39,32 +39,32 @@ If (!(Test-Path $HOMEREPO/opt)) {New-Item $HOMEREPO/opt/bin -ItemType Directory 
 
 # Set .dotfiles repo setting
 $curpath=$PWD
-cd $HOMEREPO
+Set-Location $HOMEREPO
 git config user.name "User"
 git config user.email waterns@users.noreply.github.com
 git config push.default matching
-cd $curpath
+Set-Location $curpath
 
 
 # Create Powershell Profile if doesn't exist
 $ProfileFile=$PROFILE
 If (!(Test-Path (Split-Path $ProfileFile))) {
-  echo "  Profile folder not found, creating..."
+  Write-Output "  Profile folder not found, creating..."
   New-Item $(Split-Path $ProfileFile) -ItemType Directory > $null
 }
 if (!(Test-Path $ProfileFile)) {
-  echo "NOTE: $ProfileFile not found, creating!"
+  Write-Output "NOTE: $ProfileFile not found, creating!"
   touch $ProfileFile
-  echo ""
+  Write-Output ""
 }
 
 
 
 #Add Reference to our dotfile profile to each Powershell profile we use
 $ProfileDotFile='$HOME\.dotfiles\powershell\profile-powershell.ps1'
-if (!(Get-Content "$ProfileFile" | Where {$_ -like "*$ProfileDotFile*"})) {
-  echo 'NOTE: Powershell Profile found, but missing reference to our dotfiles repo, adding!'
-  echo ". $ProfileDotFile" >> $ProfileFile
+if (!(Get-Content "$ProfileFile" | Where-Object {$_ -like "*$ProfileDotFile*"})) {
+  Write-Output 'NOTE: Powershell Profile found, but missing reference to our dotfiles repo, adding!'
+  Write-Output ". $ProfileDotFile" >> $ProfileFile
 }
 
 # Create VS Code Powershell Profile if doesn't exist
@@ -74,13 +74,13 @@ If (Test-Path "~\Documents") {
 
   $VSCodeProfileFile="~\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1"
   if (!(Test-Path $VSCodeProfileFile)) {
-    echo "NOTE: $VSCodeProfileFile not found, creating!"
+    Write-Output "NOTE: $VSCodeProfileFile not found, creating!"
     touch $VSCodeProfileFile
-    echo ""
+    Write-Output ""
   }
-  if (!(Get-Content "$VSCodeProfileFile" | Where {$_ -like "*$ProfileDotFile*"})) {
-    echo 'NOTE: VSCode Powershell Profile found, but missing reference to our dotfiles repo, adding!'
-    echo ". $ProfileDotFile" >> $VSCodeProfileFile
+  if (!(Get-Content "$VSCodeProfileFile" | Where-Object {$_ -like "*$ProfileDotFile*"})) {
+    Write-Output 'NOTE: VSCode Powershell Profile found, but missing reference to our dotfiles repo, adding!'
+    Write-Output ". $ProfileDotFile" >> $VSCodeProfileFile
   }
 
   # VS Code settings.json
@@ -88,22 +88,22 @@ If (Test-Path "~\Documents") {
   $VScodeSettingsdir="$env:APPDATA\Code\User\"
   $VSCodeSettingsFile="$env:APPDATA\Code\User\settings.json"
   If (-NOT (Test-Path "$VScodeSettingsdir")) {
-    echo "No vscode user dir found, creating"
+    Write-Output "No vscode user dir found, creating"
     mkdir $VScodeSettingsdir > $null
   }
   If (Test-Path "$VSCodeSettingsFile") {
 
     If (-NOT (Get-Item "$VSCodeSettingsFile" | Select-Object -ExpandProperty Target) -like "*VSCodeSettingsRepoFile*") {
       Write-Output "Found existing VScode file, removing"
-    Remove-Item "$VSCodeSettingsFile"
-  }
-  
+      Remove-Item "$VSCodeSettingsFile"
+    }
+
   }
   
   If (-NOT (Test-Path "$VSCodeSettingsFile")) {
-  # Requires Admin Permissions
+    # Requires Admin Permissions
     Write-Output "Linking $VSCodeSettingsFile to $VSCodeSettingsRepoFile"
-  New-Item -ItemType SymbolicLink -Path "$VSCodeSettingsFile" -Value "$VSCodeSettingsRepoFile" > $null
+    New-Item -ItemType SymbolicLink -Path "$VSCodeSettingsFile" -Value "$VSCodeSettingsRepoFile" > $null
   }
 
 }
@@ -111,11 +111,11 @@ If (Test-Path "~\Documents") {
 
 #Perl binary: diff-so-fancy (better git diff)
 if (!(Test-Path "$HOMEREPO/opt/bin/diff-so-fancy")) {
-  echo ""; echo "Pulling down: diff-so-fancy (better git diff)"
-  wget https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy -UseBasicParsing -OutFile "$HOMEREPO/opt/bin/diff-so-fancy"
+  Write-Output ""; Write-Output "Pulling down: diff-so-fancy (better git diff)"
+  Invoke-WebRequest https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy -UseBasicParsing -OutFile "$HOMEREPO/opt/bin/diff-so-fancy"
 } elseif ($update) {
-  echo ""; echo "--Updating diff-so-fancy"
-  wget https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy -UseBasicParsing -OutFile "$HOMEREPO/opt/bin/diff-so-fancy"
+  Write-Output ""; Write-Output "--Updating diff-so-fancy"
+  Invoke-WebRequest https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy -UseBasicParsing -OutFile "$HOMEREPO/opt/bin/diff-so-fancy"
 }
 
 
@@ -126,13 +126,13 @@ if (!(Test-Path $HOMEREPO\opt\lastupdate -Type Leaf)) {
 	((Get-Date -UFormat '%A %Y-%m-%d %I:%M:%S %p ')+(Get-TimeZone).ID) >> $HOMEREPO\opt\lastupdate
 }
 elseif ($update) {
-	echo ""
-	echo "Updating last update time file with current date"
+	Write-Output ""
+	Write-Output "Updating last update time file with current date"
 	[int](Get-Date (Get-Date).ToUniversalTime() -UFormat %s) > $HOMEREPO\opt\lastupdate
 	((Get-Date -UFormat '%A %Y-%m-%d %I:%M:%S %p ')+(Get-TimeZone).ID) >> $HOMEREPO\opt\lastupdate
 }
 
 if ($update) {
-	echo ""
-	echo "UPDATING Completed!"
+	Write-Output ""
+	Write-Output "UPDATING Completed!"
 }
