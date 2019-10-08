7805136ffc49b245ddc6fbd272e8bb16b59823b2
User <waterns@users.noreply.github.com>
2019-10-07 21:33:07 -0600

    posix - init: Change to AND for ssh file logic

--- init_posix.sh
+++ init_posix.sh
@@ -116,7 +116,7 @@ cd "$HOMEREPO" || exit
 git config user.name "User"
 git config user.email waterns@users.noreply.github.com
 git config push.default matching
-if [ ! -f ~/.ssh/id_rsa ] || [ ! -f ~/.ssh/WaterNS ]; then
+if [ ! -f ~/.ssh/id_rsa ] && [ ! -f ~/.ssh/WaterNS ]; then
   echo "Creating ~/.ssh/WaterNS"
   ssh-keygen -t rsa -b 4096 -f ~/.ssh/WaterNS -N ""
 fi
