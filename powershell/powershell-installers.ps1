cf2679d4c0ce1c86e1a36498286a238d98fb50ba
User <waterns@users.noreply.github.com>
2020-08-06 19:02:31 -0600

    powershell: installers - Create install-generic-chocolatey(), switch less installer to use that

--- powershell/powershell-installers.ps1
+++ powershell/powershell-installers.ps1
@@ -1,3 +1,42 @@
+Function install-generic-chocolatey {
+  param(
+      [Parameter(Mandatory=$true)]
+      [string] $pkgname,
+
+      [string] $executablename = $pkgname
+  )
+
+  if (!(Check-Command $executablename)) {
+    if ((Check-OS) -like "*win*") {
+      "NOTE: $pkgname not found, availing into dotfiles bin"
+      "------------------------------------------------"
+      [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+      $local:latest="https://chocolatey.org/api/v2/package/$pkgname"
+
+      "Downloading $pkgname..."
+      mkdir -p "$HOME/.dotfiles/opt/tmp" | Out-Null
+      Powershell-FileDownload "$latest" -o "$HOME/.dotfiles/opt/tmp/$pkgname.zip"
+
+      Expand-Archive -LiteralPath "$HOME/.dotfiles/opt/tmp/$pkgname.zip" -DestinationPath "$HOME/.dotfiles/opt/tmp/$pkgname"
+
+      if (Test-Path "$HOME/.dotfiles/opt/tmp/$pkgname/tools/$executablename*.zip") {
+        $local:archive = (Get-ChildItem "$HOME/.dotfiles/opt/tmp/$pkgname/tools/$executablename*.zip")
+        Expand-Archive -LiteralPath $archive -DestinationPath "$HOME/.dotfiles/opt/tmp/$pkgname/tools"
+      }
+
+      $local:binary = Get-ChildItem "$HOME/.dotfiles/opt/tmp/$pkgname/tools" -Recurse -Filter "$executablename.exe"
+      Move-Item $binary.FullName "$HOME/.dotfiles/opt/bin/"
+      Remove-Item -Path "$HOME/.dotfiles/opt/tmp" -Recurse
+
+      if (Check-Command $executablename) {
+        "GOOD - $pkgname is now available"
+      } else {
+        "BAD - $pkgname doesn't seem to be available"
+      }
+    }
+  }
+}
+
 Function install-jq {
   if (!(Check-Command jq)) {
     if ((Check-OS) -like "*win*") {
@@ -63,25 +102,7 @@ Function install-shfmt {
 Function install-less {
   if (!(Check-Command less)) {
     if ((Check-OS) -like "*win*") {
-      "NOTE: less not found, availing into dotfiles bin"
-      "------------------------------------------------"
-      [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-      $local:latest="https://chocolatey.org/api/v2/package/Less"
-
-      "Downloading less..."
-      mkdir -p "$HOME/.dotfiles/opt/tmp" | Out-Null
-      Powershell-FileDownload "$latest" -o "$HOME/.dotfiles/opt/tmp/less.zip"
-
-      Expand-Archive -LiteralPath "$HOME/.dotfiles/opt/tmp/less.zip" -DestinationPath "$HOME/.dotfiles/opt/tmp/less"
-
-      Move-Item "$HOME/.dotfiles/opt/tmp/less/tools/less.exe" "$HOME/.dotfiles/opt/bin/"
-      Remove-Item -Path "$HOME/.dotfiles/opt/tmp" -Recurse
-
-      if (Check-Command less) {
-        "GOOD - less is now available"
-      } else {
-        "BAD - less doesn't seem to be available"
-      }
+      install-generic-chocolatey less
     }
   }
 }
