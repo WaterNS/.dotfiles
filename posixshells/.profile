5f56e95d9d6ca530fa2c0f0c9639371313ae4b8d
User <waterns@users.noreply.github.com>
2022-04-11 00:36:47 -0600

    posix - Identify OS (better uname)   A - posixshells/posix_id_os.sh   A - posixshells/posix_id_shell.sh * .profile - reference new posix OS ideaing scripts

--- posixshells/.profile
+++ posixshells/.profile
@@ -1,36 +1,10 @@
 #!/bin/sh
 
-#Identify running shell
-## busybox hacky solution:
-whichShellRunning=$(exec 2>/dev/null; readlink "/proc/$$/exe")
-case "$whichShellRunning" in
-  */busybox) BUSYBOX_VERSION="$(busybox | head -1 | sed 's/.*\(v[0-9\.]*\).*/\1/')"; export BUSYBOX_VERSION;;
-  *) RUNNINGSHELLVERSION=$($SHELL --version);
-esac
+# Identify Operating System (better uname)
+. ~/.dotfiles/posixshells/posix_id_os.sh
 
-if [ -n "$ZSH_VERSION" ]; then
-  export RUNNINGSHELL='zsh'
-  RUNNINGSHELLVERSION=$ZSH_VERSION
-elif [ -n "$BASH_VERSION" ]; then
-  export RUNNINGSHELL='bash'
-  RUNNINGSHELLVERSION=$BASH_VERSION
-elif [ -n "$BUSYBOX_VERSION" ]; then
-  # Calling BusyBox in itself is sometimes destructive
-  # so its often aliased to ash/sh and that can be run without exiting busybox
-  if [ "$SHELL" = "/bin/ash" ]; then
-    export RUNNINGSHELL='ash'
-  else
-    export RUNNINGSHELL="$SHELL"
-  fi
-  RUNNINGSHELLVERSION="via BusyBox $BUSYBOX_VERSION"
-else
-  if contains "$SHELL" "/sh"; then
-    export RUNNINGSHELL='sh'
-  else
-    export RUNNINGSHELL="$SHELL"
-  fi
-fi
-export RUNNINGSHELLVERSION;
+#Identify running shell
+. ~/.dotfiles/posixshells/posix_id_shell.sh
 
 ### History Stuffs
 . "$HOME/.dotfiles/posixshells/posix_history.sh"
---
+++ posixshells/posix_id_os.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# Identify Operating System (better uname)
+#OS_NAME
+#OS_VERSION
+#OS_FAMILY
+#OS_ARCH
+#OS_STRING
+unameDetails=$(uname -a);
+case "$unameDetails" in
+  *Darwin*) OS_FAMILY='Darwin';;
+  *Linux*) OS_FAMILY='Linux';;
+  *) OS_FAMILY='unknown family' && echo "Dotfiles OS_FAMILY: Wasn't able to id OS family";;
+esac
+export OS_FAMILY;
+
+case "$unameDetails" in
+  *i686*) OS_ARCH='32bit';;
+  *ARM64*) OS_ARCH='ARM64';;
+  *) OS_ARCH='unknown arch' && echo "Dotfiles OS_ARCH: Wasn't able to id OS arch";;
+esac
+export OS_ARCH;
+
+OS_NAME="unknown name"
+OS_VERSION="unknown version"
+if [ -f "/etc/os-release" ]; then
+  testOSnameLookup=$(awk -F= '$1=="Name" { print $2 ;}' /etc/os-release)
+  OS_NAME=$testOSnameLookup
+
+  testOSversionLookup=$(awk -F= '$1=="VERSION_ID" { print $2 ;}' /etc/os-release)
+  OS_VERSION=$testOSversionLookup
+elif [ "$OS_FAMILY" = "Darwin" ]; then
+  OSX_Version_File="/System/Library/CoreServices/SystemVersion.plist"
+  if [ -f "$OSX_Version_File" ] && [ -x /usr/libexec/PlistBuddy ]; then
+    osx_product_name=$(/usr/libexec/PlistBuddy -c "Print:ProductName" "$OSX_Version_File")
+    osx_product_version=$(/usr/libexec/PlistBuddy -c "Print:ProductVersion" "$OSX_Version_File")
+    #osx_product_build=$(/usr/libexec/PlistBuddy -c "Print:ProductBuildVersion" "$OSX_Version_File")
+    OS_VERSION=$osx_product_version
+  fi
+
+  AppleInstallManual="/System/Library/CoreServices/Setup Assistant.app/Contents/Resources/en.lproj/OSXSoftwareLicense.rtf"
+  if [ -f "$AppleInstallManual" ]; then
+    MacOSprettyName=$(awk '/SOFTWARE LICENSE AGREEMENT FOR macOS/' "$AppleInstallManual" | awk -F 'macOS ' '{print $NF}' | awk '{print substr($0, 0, length($0)-1)}')
+    OS_NAME="$osx_product_name $MacOSprettyName"
+  fi
+else
+  echo "Dotfiles OS_NAME: Wasn't able to id OS name"
+fi
+export OS_NAME;
+export OS_VERSION;
+
+OS_STRING="$OS_FAMILY $OS_NAME $OS_VERSION $OS_ARCH";
+export OS_STRING;
+echo "$OS_STRING";
