50172fd4d8fde7b105d124052e37149f2ef6218d
User <waterns@users.noreply.github.com>
2025-02-07 21:29:25 -0700

    Powershell - Fix some Visual Studio variable names

--- powershell/powershell-visualstudio.ps1
+++ powershell/powershell-visualstudio.ps1
@@ -2,28 +2,30 @@
 
 if (Check-Command vswhere) {
   if (!(Check-Command msbuild)) {
-    $path = vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
-    if ($path) {
-      Function AliasMSBuild {& $path $args}
+    $msBuildPath = vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
+    if ($msBuildPath) {
+      Function AliasMSBuild {& $msBuildPath $args}
       Set-Alias msbuild AliasMSBuild
     }
   }
 
   if (!(Check-Command vstest)) {
-    $path = vswhere -latest -products * -requiresAny -property installationPath
-    $path = join-path $path 'Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe'
-    if ($path) {
-      Function AliasVSTest {& $path $args}
+    $vsTestPath = vswhere -latest -products * -requiresAny -property installationPath
+    $vsTestPath = join-path $vsTestPath 'Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe'
+    if ($vsTestPath) {
+      Function AliasVSTest {& $vsTestPath $args}
       Set-Alias vstest AliasVSTest
     }
   }
 
   if (!(Check-Command vsDevCmd)) {
-    $installationPath = vswhere -prerelease -latest -property installationPath
-    if ($installationPath -and (test-path "$installationPath\Common7\Tools\vsdevcmd.bat")) {
+    $vsDevCmdPath = vswhere -prerelease -latest -property installationPath
+    if ($vsDevCmdPath -AND (Test-Path "$vsDevCmdPath\Common7\Tools\vsdevcmd.bat")){
+      $vsDevCmdPath = "$vsDevCmdPath\Common7\Tools\"
+
       Function AliasVSDevCmd {
         echo "Setting/Entering Visual Studio Dev Environment variables"
-        & "${env:COMSPEC}" /s /c "`"$installationPath\Common7\Tools\vsdevcmd.bat`" -no_logo && set" | foreach-object {
+        & "${env:COMSPEC}" /s /c "`"$vsDevCmdPath\vsdevcmd.bat`" -no_logo && set" | foreach-object {
           $name, $value = $_ -split '=', 2
           set-content env:\"$name" $value
         }
@@ -31,7 +33,7 @@ if (Check-Command vswhere) {
       Function AliasVSDevPS {
         $currentPath=$PWD
         echo "Entering Visual Studio Dev Environment (Powershell) ...`n"
-        & $installationPath\Common7\Tools\Launch-VsDevShell.ps1
+        & "$vsDevCmdPath\Launch-VsDevShell.ps1"
         Set-Location "$currentPath"
       }
       Set-Alias vsDevCmd AliasVSDevPS
